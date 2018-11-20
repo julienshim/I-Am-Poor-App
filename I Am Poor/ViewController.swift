0843b4e46ff76b6771b0f0e650ee2bcab34e0619
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-20 13:17:17 -0800

    first commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Julien Shim on 11/20/2018.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
